2f0beb5486e70d41746d56c988d4a2a0d571d3ac
anah028 <116830196+anah028@users.noreply.github.com>
2023-05-03 16:30:31 -0300

    Hys

---
+++ src/main/java/com/example/demo/Repository/Rhys.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.Repository;
+
+import com.example.demo.Entity.hys;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author anilu
+ */
+
+@Repository
+public interface Rhys extends JpaRepository<hys, Integer>{
+    
+    Optional<hys> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+    
+}
